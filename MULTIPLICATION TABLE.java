6b5959d010996e529e44508e1f8572f331c46e17
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-21 20:28:44 +0530

    Update and rename Table.java to MULTIPLICATION TABLE.java

--- MULTIPLICATION TABLE.java
+++ MULTIPLICATION TABLE.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
+        Scanner s=new Scanner(System.in);
 	System.out.println("Enter Number To Print Table");
 	int a=s.nextInt();
 	int i=1;

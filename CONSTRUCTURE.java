b564a92f1d59e4819fbff423280b1e95ae83fee1
Rahultimbaliya14 <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-21 19:11:46 +0530

    Update and rename Constructor.java to CONSTRUCTURE.java

--- CONSTRUCTURE.java
+++ CONSTRUCTURE.java
@@ -1,15 +1,20 @@
 public class Main {
 	int a,b;
-	public Main()
+	public Main() //DEFAULT CONSTRUCTOR
 	{
 		a=20;
 		b=30;
 	}
-	public Main(int A,int B)
+	public Main(int A,int B)//PERAMETERIZE CONSTRUCTOR
 	{
 		a=A;
 		b=B;
 	}
+        Public Main(Main m)//COPY CONSTRUCTOR
+        {
+            this.a=a;
+            this.b=b;
+        }
 	public void display()
 	{
 		System.out.printf("First Number=%d\nSecond Number=%d\n",a,b);

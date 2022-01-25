52017bbdb8abba6aba909b22db9369fe9f7f5419
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-25 10:21:27 +0530

    Add files via upload

---
+++ TRANSPOSE OF METRIX.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class TRANSPOSEOFMETRIX {
+	public static void main(String[] args) {
+		int row,column;
+		Scanner s=new Scanner(System.in);
+		System.out.println("TRANSPOSE OF METRIX");
+		System.out.println();
+		System.out.println("Enter The number of row");
+		row=s.nextInt();
+		System.out.println("Enter Number of column");
+		column=s.nextInt();
+		int [][]ori=new int[row][column];
+		System.out.println("************************");
+		System.out.println("ENTER THE METRIX");
+		System.out.println("************************");
+		for(int i=0;i<row;i++)
+		{
+			
+			for(int j=0;j<column;j++)
+			{
+				System.out.println("Enter=["+i+"]["+j+"]");
+				ori[i][j]=s.nextInt();
+;			}
+		}
+		System.out.println();
+		System.out.println("**********************");
+		System.out.println("ORIGINAL METRIX");
+		System.out.println("**********************");
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<column;j++)
+			{
+				System.out.print(+ori[i][j]+"   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("**********************");
+		System.out.println("TRANSPOSE METRIX");
+		System.out.println("**********************");
+		for( int i=0;i<column;i++)
+		{
+			for(int j=0;j<row;j++)
+			{
+				System.out.print(+ori[j][i]+"   ");
+			}
+			System.out.println();
+		}
+		
+	}
+}

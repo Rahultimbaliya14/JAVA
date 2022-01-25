9b0a77f578332c3e91cf536d35dd74dbb80c673d
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-25 14:35:39 +0530

    Update and rename Armstrong.java to  ARMSTRONG.java

--- ARMSTRONG.java
+++ ARMSTRONG.java
@@ -1,31 +1,15 @@
-class Armstrong{
-
+class ARMSTRONG{
 	public static void main(String args[]){
-
-	
-	int n=153,r,sum=0,temp;
-
-			
+	int n=153,r,sum=0,temp;			
 	temp=n;
-
 	while(n>0){
 		r=n%10;
 		sum=sum+(r*r*r);
 		n=n/10;
 		}
-	
 	if(temp==sum)
-
 	System.out.println(temp + " is armstrong number");
-
 	else
-
 	System.out.println(temp + " is not armstrong number");
-
 	}
-
-
-
-
-
 }

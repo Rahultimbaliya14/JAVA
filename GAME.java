cd51ca6aa448e76d90a854e616341c08e56ce3f5
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-02-04 12:50:39 +0530

    Update and rename Game.java to GAME.java

--- GAME.java
+++ GAME.java
@@ -1,10 +1,10 @@
+//CREATED BY RAHUL TIMBALIYA 
 import java.util.*;
-public class Main {
+public class GAME {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String n;
 		char c='+';
-		int Stop;
 		System.out.println("*******************");
 		System.out.println("Enter The Word");
 		System.out.println("*******************");
@@ -23,24 +23,24 @@ public class Main {
 		}
 		System.out.println();
 		System.out.println("***************************");
-        while(c !='!')
-        {
-        	System.out.println();
-        	System.out.println("Enter Character And Press ! To Exit");
-        	c=s.next().charAt(0);
-        	System.out.println();
-        	for(int i=0;i<n.length();i++)
-        	{
+                while(c !='!')
+               {
+        	     System.out.println();
+        	     System.out.println("Enter Character And Press ! To Exit");
+        	     c=s.next().charAt(0);
+        	     System.out.println();
+        	     for(int i=0;i<n.length();i++)
+        	     {
         		if(a[i]==c)
         		{
         			b[i]=c;
         		}
-        	}
-        	for( char r:b)
-        	{
+        	      }
+        	     for( char r:b)
+        	     {
         		System.out.print(r+" ");
-        	}
-        	System.out.println();
-        }
-		}
+        	     }
+         	     System.out.println();
+                 }
+                 }
 }

b92f234f75f7ac0dd96b33a7ba99f42deb91d623
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-02-16 09:42:22 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,16 @@
+public class Circle{
+	int radius;.
+	static int count;
+	Circle()
+	{
+		System.out.println("Instance is created using default constructor");
+		count++;
+	}
+	Circle(int a)
+	{
+		radius=a;
+	}
+	public static void main(String[] args) {
+	  Circle c=new Circle()
+	}
+}

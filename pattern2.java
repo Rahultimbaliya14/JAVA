c2c5c36722777800574e3d3d5254f16e9ea98ab1
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-02-24 19:52:26 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		int j,i,n;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter Number");
+		n=s.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			for( j=n;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+			for( j=1;j<=i;j++)
+			{
+				System.out.print(+j);
+			}
+			for( j=j-2;j>=1;j--)
+			{
+				
+				System.out.print(+j);
+			}
+			System.out.println();
+			
+		}
+	}
+}
